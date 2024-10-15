826e87ef4530173109ddf00d739ff1ea156fe9a0
vanechek1 <ivankicherov@mail.ru>
2024-10-16 01:34:34 +0500

    reaction on pressing the button

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ from pymorphy2 import MorphAnalyzer
 bot = Bot('8075407431:AAFAhn5On7QgbH3a571MBa4ersEnC8RFDLw')
 dp = Dispatcher(bot)
 
+
+@dp.message_handler(text=['Хочу задать вопрос'])
+async def question(message: types.Message):
+    await message.reply('Много хочешь, подумай сам и не задавай тупых вопросов')
+
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await message.reply('Привет! Я - бот-помощник, можешь задать мне вопрос, а я попробую на него ответить!')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.InlineKeyboardButton('Хочу задать вопрос', callback_data='question')
+    markup.row(btn1)
+    await message.reply('Привет! Я - бот-помощник, можешь задать мне вопрос, а я попробую на него ответить!', reply_markup=markup)
 
 @dp.message_handler(commands=['inline'])
 async def info(message: types.Message):
@@ -18,6 +26,13 @@ async def info(message: types.Message):
     markup.add(types.InlineKeyboardButton('Site', callback_data='сайта нет'))
     markup.add(types.InlineKeyboardButton('Hello', callback_data='hello'))
     await message.reply('Hello', reply_markup=markup)
+
+@dp.callback_query_handler(lambda c: c.data in ['question'])
+async def callback(call: types.CallbackQuery):
+    if call.data == 'question':
+        await bot.answer_callback_query(call.id)
+        await bot.send_message(call.from_user.id, 'Хороший вопрос, подумай сам')
+
 
 @dp.message_handler(commands=['reply'])
 async def reply(message: types.Message):
